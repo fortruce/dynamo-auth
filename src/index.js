e7b6a2a8eb2ff6185eb590df77b88d2e86c3e0a0
Joseph Rollins <joseph@socialtables.com>
2016-01-19 20:46:57 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import restify from "restify";
+
+const server = restify.createServer();
+server.get('/hello/:name', (req, res) => {
+	res.send(`hello ${req.params.name}`);
+});
+
+server.listen(8080, function() {
+	console.log(`${server.name} listening at ${server.url}`);
+});
