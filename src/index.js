d68bc1f179b2851d3aca3238c5ac9c5c81e272d0
fortruce <rollins.joseph@gmail.com>
2016-01-20 19:36:35 -0500

    Test server env vars

--- src/index.js
+++ src/index.js
@@ -6,10 +6,9 @@ import restify from "restify";
 
 const server = restify.createServer();
 server.get('/hello/:name', (req, res) => {
-	res.send(`hello ${req.params.name}`);
+	res.send(`${process.env.EXAMPLE_ENV_VAR} ${req.params.name}`);
 });
 
 server.listen(process.env.PORT, function() {
 	console.log(`${server.name} listening at ${server.url}`);
-	console.log(process.env.EXAMPLE_ENV_VAR);
 });
