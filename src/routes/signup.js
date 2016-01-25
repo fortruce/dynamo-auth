565bc0759a87bdf8de87b01b64f369eb51cfe584
fortruce <rollins.joseph@gmail.com>
2016-01-24 22:00:10 -0500

    Create user in signup handler
    Also, prefer `next` over `res.send` for errors in handlers.

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,22 +1,33 @@
 const restify = require("restify");
+const logger = require("../logger");
+const models = require("../models");
 
 function required(accessor, properties) {
   return (req, res, next) => {
     const has = req[accessor];
     if (!has) {
-      res.send(new restify.errors.BadRequestError(`Missing ${accessor} on request`));
+      return next(new restify.errors.BadRequestError(`Missing ${accessor} on request`));
     }
     properties.forEach(prop => {
       if (!has.hasOwnProperty(prop)) {
-        res.send(new restify.errors.BadRequestError(`Missing required ${accessor} parameter ${prop}`));
+        return next(new restify.errors.BadRequestError(`Missing required ${accessor} parameter ${prop}`));
       }
     });
     next();
   };
 }
 
-function signupHandler(req, res) {
-  res.json(201, { email: req.body.email });
+function signupHandler(req, res, next) {
+  models.user.create(req.body.email, req.body.password, (err, user) => {
+    if (err) {
+      logger.error(err);
+      return next(new restify.InternalServerError("Error creating user"));
+    }
+    res.json(201, {
+      email: user.email,
+      id: user.id
+    });
+  });
 }
 
 module.exports = server => {
--- test/routes.js
+++ test/routes.js
@@ -1,33 +1,57 @@
 const test = require("tape");
 const request = require("supertest")(require("../src/server"));
+const db = require("./db");
+
+function onlyKeys(t, keys, obj) {
+  keys.every(key => {
+    t.ok(obj.hasOwnProperty(key), `Object missing property ${key}`);
+  });
+  Object.keys(obj).forEach(key => {
+    if (keys.indexOf(key) === -1) {
+      t.fail(`Object has unwanted property ${key}`);
+    }
+  });
+}
 
 test("routes", t => {
   t.test("/signup", t => {
     t.test("should require email and password", t => {
       request.post("/signup")
         .expect(400)
-        .end(err => t.error(err, "bad request"));
+        .end(err => t.error(err));
 
       request.post("/signup")
-        .send({ email: "", password: "" })
-        .expect(201)
-        .end(err => t.error(err, "good request"));
+        .send({ email: "user@example.com" })
+        .expect(400)
+        .end(err => t.error(err));
+
+      request.post("/signup")
+        .send({ password: "password" })
+        .expect(400)
+        .end(err => t.error(err));
 
       t.end();
     });
 
     t.test("should create user", t => {
-      request.post("/signup")
-        .send({ email: "user@example.com", password: "password" })
-        .expect(201)
-        .end((err, res) => {
-          t.error(err);
-          t.ok(res.body.email);
-          t.notOk(res.body.hash);
-          t.notOk(res.body.password);
-          // TODO add tests that spy model.users.create
-          t.end();
-        });
+      db.with([require("../schemas/users")], (err, done) => {
+        request.post("/signup")
+          .send({ email: "user@example.com", password: "password" })
+          .expect(201)
+          .end((err, res) => {
+            t.error(err);
+            onlyKeys(t, ["email", "id"], res.body);
+
+            db.table("Users")
+              .index("EmailIndex")
+              .where("email").eq(res.body.email)
+              .query((err, data) => {
+                t.error(err);
+                t.equal(data.length, 1);
+                done(t.end);
+              });
+          });
+      });
     });
   });
 });
