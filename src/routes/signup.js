0bcc5187aa543578979894fe1e386a46ddb10582
fortruce <rollins.joseph@gmail.com>
2016-01-26 20:59:00 -0500

    Add email and password validators

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,4 +1,5 @@
 const restify = require("restify");
+const validator = require("email-validator");
 const logger = require("../logger");
 const models = require("../models");
 
@@ -15,6 +16,24 @@ function required(accessor, properties) {
     });
     next();
   };
+}
+
+function validateEmail(req, res, next) {
+  if (validator.validate(req.body.email)) {
+    return next();
+  }
+  return next(new restify.BadRequestError(`Invalid email`));
+}
+
+const PASSWORD_MIN_LENGTH = 8;
+const PASSWORD_MAX_LENGTH = 64;
+function validatePassword(req, res, next) {
+  const tooShort = req.body.password.length < PASSWORD_MIN_LENGTH;
+  const tooLong = req.body.password.length > PASSWORD_MAX_LENGTH;
+  if (tooShort || tooLong) {
+    return next(new restify.BadRequestError(`Password must be between ${PASSWORD_MIN_LENGTH} and ${PASSWORD_MAX_LENGTH} characters`));
+  }
+  return next();
 }
 
 function userDoesNotExist(req, res, next) {
@@ -46,6 +65,8 @@ function createUser(req, res, next) {
 module.exports = server => {
   server.post("/signup", [
     required("body", ["email", "password"]),
+    validateEmail,
+    validatePassword,
     userDoesNotExist,
     createUser
   ]);
--- test/routes.js
+++ test/routes.js
@@ -30,6 +30,60 @@ test("/signup should require email and password", t => {
     .end(err => t.error(err));
 });
 
+test("setup db", t => db.setup("users", t.end));
+test("/signup should validate email", t => {
+  const emails = {
+    201: [
+      "foo.bar@baz.com",
+      "foo@gmail.com",
+      "xyz+test@cookies.org"
+    ],
+    400: [
+      "xyz",
+      "",
+      "foo@",
+      "foo@bar"
+    ]
+  };
+  const total = Object.keys(emails).map(k => emails[k].length).reduce((a,b) => a + b);
+  t.plan(total);
+  Object.keys(emails).forEach(code => {
+    emails[code].forEach(email => {
+      request.post("/signup")
+        .send({ email: email, password: "password" })
+        .expect(parseInt(code))
+        .end(err => t.error(err, `${email} should ${code}`));
+    });
+  });
+});
+
+test("/signup should validate password", t => {
+  const valid = [
+    "testPass$!@ASD",
+    "minimum8",
+    "maximum64aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+  ];
+  const invalid = [
+    "",
+    "minIs8-",
+    "maximum64aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+  ];
+  t.plan(valid.length + invalid.length);
+  valid.forEach((pass, i) => {
+    request.post("/signup")
+      .send({ email: `valid${i}@example.com`, password: pass })
+      .expect(201)
+      .end(err => t.error(err, `password ${pass} should be valid`));
+  });
+  invalid.forEach((pass, i) => {
+    request.post("/signup")
+      .send({ email: `invalid${i}@example.com`, password: pass })
+      .expect(400)
+      .end(err => t.error(err, `password ${pass} should be invalid`));
+  });
+});
+test("teardown", t => db.teardown(t.end));
+
 test("setup db", t => db.setup([require("../schemas/users")], t.end));
 test("/signup should create user", t => {
   request.post("/signup")
